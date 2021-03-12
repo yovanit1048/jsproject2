5d3c65d79952ace83ee1f32be8de320331bd104a
Yovani Tellez <yovanit@hstat.org>
2021-03-12 16:37:03 +0000

    update repo

--- script.js
+++ script.js
@@ -4,24 +4,26 @@ $.ajax({
     success: function(data) {
 
 
-var welecoming = alert("Hello, this is the covid meal location database.")
+var welecoming = alert("Hello, this is the covid meal location database.")//The topic
 
-function goldEditiongames(){ prompt("Choose from the following options using numbers: 1. City 2. District.");
+function goldEditiongames(){ prompt("Choose from the following options using numbers: 1. City 2. District.");//This gives user choice to choose betwwen city and district.
 
 
 if (goldEditiongames === 1) {
   cityManchester(); 
-} else if (goldEditiongames === 2) { 
+} 
+else if (goldEditiongames === 2) { 
   districtGood(); 
 }
 }
 
-goldEditiongames();
+goldEditiongames();// Calls function for the prompt to show up
 
 
-function cityManchester(){
+function cityManchester(){//the section for user's choice
 var freeMeals = [];
-var theFreemeal = prompt("Which city in New York are you looking for, to get free meal?(First word must be Capitalized!)"); //user can pick from place they want their lunch
+var theFreemeal = prompt("Which city in New York are you looking for, to get free meal?(First word must be Capitalized!)");
+var aleRt = alert("These are the schools that have free meals");//user can pick from place they want their lunch
 for (i = 0; i < data.length; i++) {
   if(data[i].city == undefined){
     freeMeals.push("None".split(", ")); // 
@@ -33,7 +35,7 @@ for (i = 0; i < data.length; i++) {
         for (i = 0; i < data.length; i++) {
           for(var j = 0; j < freeMeals[i].length; j++){
             if (freeMeals[i][j] == theFreemeal){
-              document.write("These school have free food:" + data[i].schoolname + ", " + data[i].siteaddress); //gives the data of the school names and the streeet
+              document.write(data[i].schoolname + "<br>, " + data[i].siteaddress); //gives the data of the school names and the streeet
               }
           }
      }
@@ -41,7 +43,7 @@ for (i = 0; i < data.length; i++) {
 
 cityManchester();
 
-function districtGood(){
+function districtGood(){// the second section for user's choice
 var districtlunch = [];
 var userDist = prompt("What your choice of District?(Numbers Only!)");
 for (i = 0; i < data.length; i++) {
@@ -55,7 +57,7 @@ for (i = 0; i < data.length; i++) {
       for (i = 0; i < data.length; i++){
         for (var j = 0; j < userDist[i].length; j++){
           if (districtlunch[i][j] == userDist){
-            document.write(data[i].schoolname + ", ");
+            document.write(data[i].schoolname + "<br>, ");
           }
           }
       }
